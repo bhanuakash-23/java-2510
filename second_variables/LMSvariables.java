3cb95a34dfcc5c47d4c13c4f320f225fc4694ed5
bhanuakash-23 <2200031443@kluniversity.in>
2025-09-21 23:39:12 +0530

    Variables and Data types

---
+++ second_variables/LMSvariables.java
@@ -0,0 +1,18 @@
+package second_variables;
+
+public class LMSvariables {
+    public static void main(String[] args) {
+        int studentId = 12345;
+        String studentName = "Bhanu";
+        String studentEmail = "iam19anyway@gmail.com";
+        char studentGender = 'M';
+        long studentPhone = 987654;
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Email: " + studentEmail);
+        System.out.println("Student gender: " + studentGender);
+        System.out.println("Student Phone: " + studentPhone);
+
+    }
+
+}
---
+++ second_variables/variablesDemo.java
@@ -0,0 +1,17 @@
+package second_variables;
+public class variablesDemo {
+    static short price = 350;
+    char textbrand = 'h';
+    String brand = "HIGHLANDER";
+    float ratings = 4.2f;
+    public static void main(String[] args) {
+    int somedatanew = 100;
+    System.out.println(somedatanew);
+    System.out.println(price);
+        
+
+
+        
+    }
+
+}
